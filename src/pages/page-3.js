95f7dc3cf757855e2c32f4aa38d49b64ffcf07cf
matei.eduard <eduardmatei0@yahoo.com>
2019-09-09 18:57:58 +0300

    init commit

---
+++ src/pages/page-3.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+import Layout from "../components/layout"
+
+const ThridPage = () => (
+  <Layout>
+    <h1>Hi from the third page</h1>
+  </Layout>
+)
+
+export default ThridPage
